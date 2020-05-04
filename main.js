186c0dbe49fc9d47780c2d0719e1b643cd436b0c
Laura Beerden <60176579+BLaura1995@users.noreply.github.com>
2020-05-04 09:27:44 +0200

    Update

--- main.js
+++ main.js
@@ -5,25 +5,5 @@ $(document).ready(function() {
         $('#logo').css({ transform: 'rotate(' + theta + 'rad)' });
         return;
     });
-    //Appear
-    /* Every time the window is scrolled ... */
-    $(window).scroll(function() {
-
-        /* Check the location of each desired element */
-        $('.section1').each(function(i) {
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
-
-                $(this).animate({ 'opacity': '1' }, 900);
-
-            }
-
-        });
-
-    });
 
 });
